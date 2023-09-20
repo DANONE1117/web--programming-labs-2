15b400b00362eceae5ae6bca7daaf45c7ef3c715
Your DANONE1117 <daniilkovylin2@gmail.com>
2023-09-20 15:22:08 +0700

    вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "вэб-сервер на фласк"
+    return """
+    <!doctype html>
+    <html>
+        <head>
+            <title>Ковылин Даниил Артурович, Лабораторная работа 1</title>
+        </head>
+        <body>
+            <header>
+                НГТУ, ФБ, Лабораторная работ 1
+            </header>
+
+            <h1>web-сервер на flask</h1>
+
+            <footer>
+                &copy; Ковылин Даниил Артурович, ФБИ14, 3 курс, 2023
+            </footer>
+        </body>
+    </html>
+    """
