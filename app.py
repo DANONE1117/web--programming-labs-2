dd02e603ab5eefa6af78b3912fe57bfa4c84c3f5
Your DANONE1117 <daniilkovylin2@gmail.com>
2023-10-04 16:27:28 +0700

    меню

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
     return """
     <!doctype html>
@@ -22,3 +23,33 @@ def start():
         </body>
     </html>
     """
+@app.route('/lab1')
+def lab1():     
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>Ковылин Даниил Артурович, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+        <main>
+            <h1>Web-сервер на flask</h1>
+
+            <div>
+                Flask — фреймворк для создания веб-приложений на языке
+                программирования Python, использующий набор инструментов
+                Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+                называемых микрофреймворков — минималистичных каркасов
+                веб-приложений, сознательно предоставляющих лишь самые ба-
+                зовые возможности.
+            </div>
+        </main>
+        <footer>
+            &copy; Ковылин Даниил Артурович, ФБИ-14, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+'''
